944d15d9dedd3447b52af05d79997c18e1ff3f41
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-10 19:08:48 +0530

    customer

---
+++ FirstWebApplication/FirstWebApplication/Models/Customer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FirstWebApplication.Models
+{
+    public class Customer
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+    }
+}
---
+++ FirstWebApplication/FirstWebApplication/ViewModels/RandomMovieViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FirstWebApplication.Models;
+
+namespace FirstWebApplication.ViewModels
+{
+    public class RandomMovieViewModel
+    {
+        public Movie Movie { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
