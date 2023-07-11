b055cf1bae145895d2e462aae4e9286d9b48fd61
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-11 18:55:41 +0530

    Shown list of countries

---
+++ MVCDemo/MVCDemo/Controllers/MyHomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class MyHomeController : Controller
+    {
+        // GET: MyHome
+        public string Index(string id, string name)
+        {
+            return "Hello from MVC Application and Id - " + id + " Name - " + name;
+        }
+
+        // url = MVCDemo/getDetails
+        public string GetDetails()
+        {
+            return "get details in mvc";
+        }
+
+        // url = MVCDemo/listOfCountries
+        public ActionResult ListOfCountries()
+        {
+            ViewBag.Countries = new List<string> 
+            {
+                "India",
+                "United States",
+                "United Kingdom"
+            };
+            return View();
+        }
+    }
+}
