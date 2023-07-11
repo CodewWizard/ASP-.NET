6c418b69dab54ef8e723fe05b419e246f7c2d2eb
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-11 13:09:22 +0530

    customer-controller

---
+++ FirstWebApplication/FirstWebApplication/Controllers/CustomersController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstWebApplication.Models;
+
+namespace FirstWebApplication.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        // GET: Customers
+        public ViewResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.id == id);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            {
+                new Customer { id = 1, name = "John Smith" },
+                new Customer { id = 2, name = "Mary Williams" }
+            };
+        }
+    }
+}
