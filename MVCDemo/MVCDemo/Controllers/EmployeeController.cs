5827d0814786e1841da679a9f6c4889e88447a5a
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-18 16:52:15 +0530

    database operations

--- MVCDemo/MVCDemo/Controllers/EmployeeController.cs
+++ MVCDemo/MVCDemo/Controllers/EmployeeController.cs
@@ -10,11 +10,22 @@ namespace MVCDemo.Controllers
     public class EmployeeController : Controller
     {
         // GET: Employee
-        public ActionResult Details(int id)
+        /*public ActionResult Details(int id)
         {
             EmployeeContext empContext = new EmployeeContext();
             Employee employee = empContext.Emps.Single(e => e.Id == id);
             return View(employee);
+        }*/
+
+        public ActionResult Details()
+        {
+            Employee emp = new Employee()
+            {
+                Id = 101,
+                Name = "Madiha",
+                City = "Solapur"
+            };
+            return View(emp);
         }
     }
 }
