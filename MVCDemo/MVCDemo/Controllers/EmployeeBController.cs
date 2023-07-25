46a2d2d561d7cbb390fccf6e5eb850d239015b6b
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-25 17:15:51 +0530

    created employeeController for businessLayer

---
+++ MVCDemo/MVCDemo/Controllers/EmployeeBController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using System.Web;
+using BusinessLayer;
+
+namespace MVCDemo.Controllers
+{
+    public class EmployeeBController : Controller
+    {
+        // GET: EmployeeB
+        public ActionResult Index()
+        {
+            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+
+            return View(employees);
+        }
+    }
+}
