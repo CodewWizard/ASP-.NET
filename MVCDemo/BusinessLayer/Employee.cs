38e40de603ee6ec4b79ebd4829d609cabe0dd2a6
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-25 17:14:49 +0530

    created BusinessLayer

---
+++ MVCDemo/BusinessLayer/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Employee
+    {
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public string EmpCity { get; set; }
+        public string EmpGender { get; set; }
+
+        public int DepartmentId { get; set; }
+    }
+}
---
+++ MVCDemo/BusinessLayer/EmployeeBusinessLayer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace BusinessLayer
+{
+    public class EmployeeBusinessLayer
+    {
+        public IEnumerable<Employee> Employees
+        {
+            get
+            {
+                string connectionString =
+                    ConfigurationManager.ConnectionStrings["EmployeeContext"].ConnectionString;
+
+                List<Employee> employees = new List<Employee>();
+
+                using (SqlConnection con = new SqlConnection(connectionString))
+                {
+                    SqlCommand cmd = new SqlCommand("spGetAllEmployees", con);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    con.Open();
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        Employee employee = new Employee();
+                        employee.EmpId = Convert.ToInt32(rdr["Id"]);
+                        employee.EmpName = rdr["Name"].ToString();
+                        employee.EmpGender = rdr["Gender"].ToString();
+                        employee.EmpCity = rdr["City"].ToString();
+                        employee.DepartmentId = Convert.ToInt32(rdr["DepartmentId"]);
+
+                        employees.Add(employee);
+                    }
+                }
+
+                return employees;
+            }
+        }
+    }
+}
