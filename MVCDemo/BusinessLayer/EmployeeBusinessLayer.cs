7fc6277dd996f90d207589f8498400bcc6c20d4e
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-27 13:38:17 +0530

    updated

--- MVCDemo/BusinessLayer/EmployeeBusinessLayer.cs
+++ MVCDemo/BusinessLayer/EmployeeBusinessLayer.cs
@@ -10,14 +10,14 @@ namespace BusinessLayer
 {
     public class EmployeeBusinessLayer
     {
-        public IEnumerable<Employee> Employees
+        public IEnumerable<Employee1> Employees
         {
             get
             {
                 string connectionString =
                     ConfigurationManager.ConnectionStrings["EmployeeContext"].ConnectionString;
 
-                List<Employee> employees = new List<Employee>();
+                List<Employee1> employees = new List<Employee1>();
 
                 using (SqlConnection con = new SqlConnection(connectionString))
                 {
@@ -27,11 +27,11 @@ namespace BusinessLayer
                     SqlDataReader rdr = cmd.ExecuteReader();
                     while (rdr.Read())
                     {
-                        Employee employee = new Employee();
-                        employee.EmpId = Convert.ToInt32(rdr["Id"]);
-                        employee.EmpName = rdr["Name"].ToString();
-                        employee.EmpGender = rdr["Gender"].ToString();
-                        employee.EmpCity = rdr["City"].ToString();
+                        Employee1 employee = new Employee1();
+                        employee.EmpId = Convert.ToInt32(rdr["EmpId"]);
+                        employee.EmpName = rdr["EmpName"].ToString();
+                        employee.EmpCity = rdr["EmpCity"].ToString();
+                        employee.EmpGender = rdr["EmpGender"].ToString();
                         employee.DepartmentId = Convert.ToInt32(rdr["DepartmentId"]);
 
                         employees.Add(employee);
@@ -39,6 +39,47 @@ namespace BusinessLayer
                 }
 
                 return employees;
+            }
+        }
+
+        public void AddEmployee(Employee1 employee)
+        {
+            string connectionString =
+            ConfigurationManager.ConnectionStrings["EmployeeContext"].ConnectionString;
+
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                SqlCommand cmd = new SqlCommand("spAddEmployee", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+
+                SqlParameter paramId = new SqlParameter();
+                paramId.ParameterName = "@EmpId";
+                paramId.Value = employee.EmpId;
+                cmd.Parameters.Add(paramId);
+
+                SqlParameter paramName = new SqlParameter();
+                paramName.ParameterName = "@EmpName";
+                paramName.Value = employee.EmpName;
+                cmd.Parameters.Add(paramName);
+
+                SqlParameter paramCity = new SqlParameter();
+                paramCity.ParameterName = "@EmpCity";
+                paramCity.Value = employee.EmpCity;
+                cmd.Parameters.Add(paramCity);
+
+                SqlParameter paramGender = new SqlParameter();
+                paramGender.ParameterName = "@EmpGender";
+                paramGender.Value = employee.EmpGender;
+                cmd.Parameters.Add(paramGender);
+
+
+                SqlParameter paramDepartmentId = new SqlParameter();
+                paramDepartmentId.ParameterName = "@DepartmentId";
+                paramDepartmentId.Value = employee.DepartmentId;
+                cmd.Parameters.Add(paramDepartmentId);
+
+                con.Open();
+                cmd.ExecuteNonQuery();
             }
         }
     }
