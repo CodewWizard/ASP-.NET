fa25cb4488898df981812e848e1294e155a64f85
Madiha Mujawar <mmujawar@cogitate.us>
2023-07-28 16:16:12 +0530

    Added [Required

--- MVCDemo/BusinessLayer/Employee1.cs
+++ MVCDemo/BusinessLayer/Employee1.cs
@@ -3,15 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace BusinessLayer
 {
     public class Employee1
     {
+        [Required]
         public int EmpId { get; set; }
+        [Required]
         public string EmpName { get; set; }
+        [Required]
         public string EmpCity { get; set; }
+        [Required]
         public string EmpGender { get; set; }
+        [Required]
         public int DepartmentId { get; set; }
     }
 }
